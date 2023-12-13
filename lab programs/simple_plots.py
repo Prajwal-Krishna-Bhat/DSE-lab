1ff266eae7c2549e2300563b526d4a2bb53e5c8f
Prajwal-Krishna-Bhat <133648728+Prajwal-Krishna-Bhat@users.noreply.github.com>
2023-12-13 03:41:25 +0000

    done

--- lab programs/simple_plots.py
+++ lab programs/simple_plots.py
@@ -1,1 +1,10 @@
 import matplotlib.pyplot as plt
+x=['MAY','JUNE','JULY','AUG','SEPT','OCT','NOV','DEC']
+guests=[23,32,27,50,22,18,11,65]
+subs=[12,10,9,15,5,2,3,12]
+plt.xlabel('visitors')
+plt.ylabel('month/year')
+plt.plot(x,guests,label='guests')
+plt.plot(x,subs,label='subscribers')
+plt.legend()
+plt.show()
