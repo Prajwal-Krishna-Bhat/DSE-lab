90a8c1f56b2b6fcade6540aebec5ca26fa8fff4a
Prajwal-Krishna-Bhat <133648728+Prajwal-Krishna-Bhat@users.noreply.github.com>
2023-12-20 10:22:59 +0530

    Create dif_plots.py
    no

---
+++ lab programs/dif_plots.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#line chart
+x=np.linspace(0,10,100)
+y=np.sin(x)
+plt.figure()
+plt.plot(x,y)
+plt.title("Line chart")
+plt.xlabel("X-axis")
+plt.ylabel("y-axis")
+
+#Bar chart
+categories=['A','B','C','D']
+values=[20,35,30,25]
+plt.figure()
+plt.bar(categories, values)
+plt.title("Bar chart")
+plt.xlabel("Categories")
+plt.ylabel("Values")
+
+#Scatter plot
+x=np.random.randn(100)
+y=np.random.randn(100)
+colors=np.random.randn(100)
+sizes=100*np.random.randn(100)
+plt.figure()
+plt.scatter(x,y,c=colors,s=sizes,alpha=0.5)
+plt.title("Scatter plot")
+plt.xlabel("X-axis")
+plt.ylabel("y-axis")
+
+#Pie chart
+sizes=[30,20,25,15,10]
+labels=['A','B','C','D','E']
+plt.figure()
+plt.pie(sizes,labels=labels,autopct='%1.1f%%')
+plt.title("Pie chart")
+
+plt.show()
